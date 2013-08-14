1c663d30bf92db248d7c5e755558dc677c62dd69
Exikle <xidstudios@hotmail.com>
2013-08-14 19:04:57 -0400

    Faded splashscreen in and out, and switched to startscreen
    Signed-off-by: Exikle <xidstudios@hotmail.com>

--- PixelArena/src/com/xidstudios/pixelarena/screens/SplashScreen.java
+++ PixelArena/src/com/xidstudios/pixelarena/screens/SplashScreen.java
@@ -1,5 +1,9 @@
 package com.xidstudios.pixelarena.screens;
 
+import aurelienribon.tweenengine.BaseTween;
+import aurelienribon.tweenengine.Tween;
+import aurelienribon.tweenengine.TweenCallback;
+import aurelienribon.tweenengine.TweenEquations;
 import aurelienribon.tweenengine.TweenManager;
 
 import com.badlogic.gdx.Gdx;
@@ -9,6 +13,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.xidstudios.pixelarena.PixelArenaMain;
+import com.xidstudios.pixelarena.tweenaccesors.SpriteTween;
 
 public class SplashScreen implements Screen {
 
@@ -26,10 +31,11 @@ public class SplashScreen implements Screen {
 	public void render(float delta) {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-//		Gdx.app.log(PixelArenaMain.LOG, "Rendering SplashScreen");
+		// Gdx.app.log(PixelArenaMain.LOG, "Rendering SplashScreen");
 
+		manager.update(delta);
 		batch.begin();
-		
+
 		splashSprite.draw(batch);
 
 		batch.end();
@@ -43,9 +49,33 @@ public class SplashScreen implements Screen {
 	@Override
 	public void show() {
 		batch = new SpriteBatch();
-		splashSprite = new Sprite(new Texture("imgs/xidstudios_splash.png"));
+		splashTexture = new Texture("imgs/xidstudios_splash.png");
+		splashSprite = new Sprite(splashTexture);
 
 		splashSprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		splashSprite.setColor(1, 1, 1, 0);
+
+		Tween.registerAccessor(Sprite.class,
+				new com.xidstudios.pixelarena.tweenaccesors.SpriteTween());
+
+		manager = new TweenManager();
+
+		TweenCallback cb = new TweenCallback() {
+
+			@Override
+			public void onEvent(int type, BaseTween<?> source) {
+				tweenCompleted();
+			}
+		};
+
+		Tween.to(splashSprite, SpriteTween.ALPHA, 1.5f).target(1)
+				.ease(TweenEquations.easeInQuad).repeatYoyo(1, 1.5f)
+				.setCallback(cb).setCallbackTriggers(TweenCallback.COMPLETE)
+				.start(manager);
+	}
+
+	protected void tweenCompleted() {
+		game.setScreen(new StartScreen(game));
 	}
 
 	@Override
