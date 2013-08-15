6e399be4846636b3ba3180a0dba74f3f5d855d5d
Exikle <xidstudios@hotmail.com>
2013-08-15 16:50:12 -0400

    Transitioned to a start screen...which doesn't have anything yet
    Signed-off-by: Exikle <xidstudios@hotmail.com>

--- PixelArena-desktop/src/com/xidstudios/pixelarena/Main.java
+++ PixelArena-desktop/src/com/xidstudios/pixelarena/Main.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "Pixel Arena || " + PixelArenaMain.PIXELARENA_VERSION;
+		cfg.title = "Pixel Arena  - Xid Studios " + PixelArenaMain.PIXELARENA_VERSION;
 		cfg.useGL20 = true;
 		cfg.width = 1080;
 		cfg.height = 720;
---
+++ PixelArena/src/com/xidstudios/pixelarena/screens/StartScreen.java
@@ -0,0 +1,47 @@
+package com.xidstudios.pixelarena.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL10;
+import com.xidstudios.pixelarena.PixelArenaMain;
+
+public class StartScreen implements Screen {
+
+	PixelArenaMain game;
+
+	public StartScreen(PixelArenaMain game) {
+		this.game = game;
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+	}
+
+	@Override
+	public void resize(int width, int height) {
+
+	}
+
+	@Override
+	public void show() {
+	}
+
+	@Override
+	public void hide() {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+}
