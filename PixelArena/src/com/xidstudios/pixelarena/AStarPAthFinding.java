b02e7e7c30719c05b214cfa713d2cf51c486e099
Exikle <dixondcunha@hotmail.com>
2013-09-07 17:44:44 -0400

    replaced the ints with Vector2 cuz theyre coordinates
    Signed-off-by: Exikle <dixondcunha@hotmail.com>

--- PixelArena/src/com/xidstudios/pixelarena/AStarPAthFinding.java
+++ PixelArena/src/com/xidstudios/pixelarena/AStarPAthFinding.java
@@ -8,19 +8,17 @@ import com.badlogic.gdx.math.Vector2;
 
 public class AStarPAthFinding {
 
-	public static void calcPAth(int fromX, int fromY, int toX,
-			int toY, Cell[][] mapCells) {
-		int startX = fromX;
-		int startY = fromY;
-		int endX = toX;
-		int endY = toY;
+	public static void calcPAth(Vector2 from, Vector2 to,
+			Cell[][] mapCells) {
+		Vector2 start = from;
+		Vector2 end = to;
 		Cell[][] cells = mapCells;
 
 		List<Vector2> openList = new ArrayList<Vector2>();
 		List<Vector2> closedList = new ArrayList<Vector2>();
 
-		closedList.add(new Vector2(startX, startY));// add start to closed list,
-													// dont need to check;
+		closedList.add(start);// add start to closed list,
+								// dont need to check;
 
 		// check if the parent is in a list
 		// if (openList.contains(new Vector2())) {
