9e55de0a8ca3bbc494335de68eb7b977e463ded6
Exikle <dixondcunha@hotmail.com>
2013-09-12 16:16:36 -0400

    Made self instructions to do pathfinding, might make a new class
    Signed-off-by: Exikle <dixondcunha@hotmail.com>

--- PixelArena/src/com/xidstudios/pixelarena/Node.java
+++ PixelArena/src/com/xidstudios/pixelarena/Node.java
@@ -10,8 +10,6 @@ public class Node {
 
 	int totalValue;
 
-	int id;
-
 	Node parentNode;
 
 	Vector2 parentVector;
@@ -23,92 +21,26 @@ public class Node {
 		this.parentNode = node;
 	}
 
-	/**
-	 * @return the hValue
-	 */
-	public int gethValue() {
-		return hValue;
-	}
-
-	/**
-	 * @param Set
-	 *            the hValue with hValue
-	 */
 	public void sethValue(int hValue) {
 		this.hValue = hValue;
 	}
 
-	/**
-	 * @return the gValue
-	 */
-	public int getMoveCost() {
-		return moveCostValue;
-	}
-
-	/**
-	 * @param Set
-	 *            the gValue with gValue
-	 */
 	public void setMoveCost(int moveCostValue) {
 		this.moveCostValue = moveCostValue;
 	}
 
-	/**
-	 * @return the totalValue
-	 */
 	public int getTotalValue() {
 		return totalValue;
 	}
 
-	/**
-	 * @param Set
-	 *            the totalValue with totalValue
-	 */
-	public void setTotalValue(int totalValue) {
-		this.totalValue = totalValue;
-	}
-
-	/**
-	 * @return the id
-	 */
-	public int getId() {
-		return id;
-	}
-
-	/**
-	 * @param Set
-	 *            the id with id
-	 */
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	/**
-	 * @return the parentNode
-	 */
-	public Node getParentNode() {
-		return parentNode;
-	}
-
-	/**
-	 * @param Set
-	 *            the parentNode with parentNode
-	 */
 	public void setParentNode(Node parentNode) {
 		this.parentNode = parentNode;
 	}
 
-	/**
-	 * @return the parentVector
-	 */
 	public Vector2 getParentVector() {
 		return parentVector;
 	}
 
-	/**
-	 * @param Set
-	 *            the parentVector with parentVector
-	 */
 	public void setParentVector(Vector2 parentVector) {
 		this.parentVector = parentVector;
 	}
--- PixelArena/src/com/xidstudios/pixelarena/PathFinding.java
+++ PixelArena/src/com/xidstudios/pixelarena/PathFinding.java
@@ -9,8 +9,25 @@ import com.badlogic.gdx.math.Vector2;
 import com.xidstudios.pixelarena.arena.Arena;
 
 /*
- * 1. check through closed list and make sure node being checked isnt on list
- * 2. check through openlist
+ * calc surrounding tiles f val
+ * Check through closed list and make sure node being checked isnt on list
+ * Check through openlist, if node in list is better than current one(new g< current g), if it is, replace parent with new one
+ * Check surrounding tiles for lowest f val
+ * Set lowest as current node
+ * remove current node from open and add to closed list
+ * repeat.
+ * 
+ * 1. Add starting node to openlist
+ * 2. Loop{
+ *  a) Look for the lowest F cost square on the open list. We refer to this as the current square.
+ *  b) Switch it to the closed list.
+ *  c) For each of the 8 squares adjacent to this current square …
+ *     - If it is not walkable or if it is on the closed list, ignore it. Otherwise do the following.           
+ *     - If it isn’t on the open list, add it to the open list. Make the current square the parent of this square. Record the F, G, and H costs of the square. 
+ *     - If it is on the open list already, check to see if this path to that square is better, using G cost as the measure. A lower G cost means that this is a better path. If so, change the parent of the square to the current square, and recalculate the G and F scores of the square. If you are keeping your open list sorted by F score, you may need to resort the list to account for the change.
+ *  d) Stop when you:
+ *     - Add the target square to the closed list, in which case the path has been found (see note below), or
+ *     - Fail to find the target square, and the open list is empty. In this case, there is no path.   
  */
 
 public class PathFinding {
@@ -45,7 +62,7 @@ public class PathFinding {
 
 	static int endPosY;
 
-	static int identity;
+	private final static int TIMES_TO_LOOP = 1;// debug variable
 
 	public static void calcPAth(Vector2 from, Vector2 to,
 			Cell[][] mapCells, Arena a) {
@@ -53,27 +70,15 @@ public class PathFinding {
 		end = to;
 		cells = mapCells;
 		arena = a;
-		// Gdx.app.log(PArena.LOG, "Lists Created");
 
 		currentNode = new Node(null, start);
-		identity = 0;
-		currentNode.setId(identity);
-		// Gdx.app.log(PArena.LOG, "Added start to openList");
-		// check squares around this and add
 		openList.clear();
 
-		for (int loop = 0; loop < 1; loop++) {
-			if ((!openList.contains(currentNode))
-					&& (!closedList.contains(currentNode))) {
+		for (int loop = 0; loop < TIMES_TO_LOOP; loop++) {
+			if (!closedList.contains(currentNode)) {
 				openList.add(currentNode);
 				int startPX = (int) currentNode.parentVector.x;
-				// Gdx.app.log(PArena.LOG, "Start X " + startPX);
 				int startPY = (int) currentNode.parentVector.y;
-				// Gdx.app.log(PArena.LOG, "Start Y " + startPY);
-
-				// Gdx.app.log("", "" + currentNode.parentV);
-
-				//
 				Gdx.app.log(PArena.LOG, "Pos "
 						+ currentNode.parentVector);
 				MIN_X = startPX - 1;
@@ -94,11 +99,8 @@ public class PathFinding {
 				endPosY = (startPY + 1 > MAX_Y) ? startPY
 						: startPY + 1;
 
-				// Check boundaries on start cell
 				for (int colNum = startPosX; colNum <= endPosX; colNum++) {
 					for (int rowNum = startPosY; rowNum <= endPosY; rowNum++) {
-						// Gdx.app.log(PArena.LOG, colNum + "," + rowNum);
-						// All the neighbors will be grid[rowNum][colNum]
 						if ((rowNum == startPY)
 								&& (colNum == startPY)) {
 							System.out.print("SS|");
@@ -130,16 +132,9 @@ public class PathFinding {
 				for (Node temp : openList) {
 					if (temp.getTotalValue() < f) {
 						f = temp.getTotalValue();
-						// System.out.println("Lowest FVal"
-						// + temp.calcFVal());
 						index = openList.lastIndexOf(temp);
-						// Gdx.app.log("Node Vals", "HVal = " + temp.hVal);
-						// Gdx.app.log("Node Vals", "GVal = " + temp.gVal);
-						// Gdx.app.log("Node Vals", "FVal = " + temp.fVal);
 					}
 				}
-				identity++;
-				currentNode.setId(identity);
 				Gdx.app.log("PathFinding",
 						"" + currentNode.getTotalValue());
 				// System.out.println("PathFinding: " + currentNode.gVal);
