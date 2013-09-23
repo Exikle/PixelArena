fd6ee800957f8742b9b71aea027f9b4d595a1ef5
Exikle <dixondcunha@hotmail.com>
2013-09-23 16:46:17 -0400

    Gonna see if i can use AndEngine
    Signed-off-by: Exikle <dixondcunha@hotmail.com>

--- PixelArena/src/com/xidstudios/pixelarena/pathfinding/ComparatorByScore.java
+++ PixelArena/src/com/xidstudios/pixelarena/pathfinding/ComparatorByScore.java
@@ -5,14 +5,19 @@ import java.util.Comparator;
 public class ComparatorByScore implements Comparator<TileInfo> {
 
 	@Override
-	//Yeah, this. This is... a replacement comparator for the PriorityQueue, of which I know little of.
-	//Maybe this works, maybe this doesn't. I don't really know yet. But it's here nonetheless.
+	// Yeah, this. This is... a replacement comparator for the PriorityQueue, of which I know little of.
+	// Maybe this works, maybe this doesn't. I don't really know yet. But it's here nonetheless.
 	public int compare(TileInfo tile1, TileInfo tile2) {
-		
-		return Integer.compare(tile1.heur + tile1.cost , tile2.heur + tile2.cost);
-		
+		int tileOneTotal = tile1.heur + tile1.cost;
+		int tileTwoTotal = tile2.heur + tile2.cost;
+		if (tileOneTotal > tileTwoTotal) {
+
+			return tileTwoTotal;
+		} else {
+
+			return tileOneTotal;
+		}
+
 	}
 
-	
-	
 }
