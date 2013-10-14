40952a0073a78d3d8091c025183f3e8ad914d86a
Exikle <xidstudios@hotmail.com>
2013-10-14 13:05:48 -0400

    Got the map to display, time to see if i can use the code to put it into the pathfinding code, atleast make the console map, find the place

--- PixelArena/src/com/xidstudios/pixelarena/arena/debug/MapDebugger.java
+++ PixelArena/src/com/xidstudios/pixelarena/arena/debug/MapDebugger.java
@@ -3,7 +3,6 @@ package com.xidstudios.pixelarena.arena.debug;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
-import com.xidstudios.pixelarena.pathfinding.testing.Map;
 
 public class MapDebugger {
 	private TiledMap tileMap;
@@ -17,21 +16,13 @@ public class MapDebugger {
 	private void print2DMap() {
 		TiledMapTileLayer layer = (TiledMapTileLayer) tileMap.getLayers()
 				.get(0);
-
-		float tileHeight = layer.getTileHeight();
-		// Gdx.app.log(PArena.LOG, "" + tileHeight);
-		float tileWidth = layer.getTileWidth();
-		// Gdx.app.log(PArena.LOG, "" + tileWidth);
-
 		int col = layer.getWidth();
 		int row = layer.getHeight();
-
-		float mapWidth = col * tileWidth;
-		float mapHeight = row * tileHeight;
 
 		cells = new Cell[col][row];
 		String[][] map = new String[col][row];
 		for (int y = 0; y < row; y++) {
+			System.out.print("|");
 			for (int x = 0; x < col; x++) {
 				cells[x][y] = new Cell();
 				cells[x][y] = layer.getCell(x, y);
@@ -39,7 +30,7 @@ public class MapDebugger {
 						.containsKey("blocked") ? "X" : "-";
 				System.out.print(map[x][y]);
 			}
-			System.out.println("");
+			System.out.println("|");
 		}
 	}
 }
